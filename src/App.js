0f9f7bee91f40f56c5a582de17de6dce2f18b84b
Samin1615088 <samin.shafkat@gmail.com>
2021-06-19 17:11:55 +0600

    main cleared

--- src/App.js
+++ src/App.js
@@ -8,10 +8,6 @@ import AlarmIcon from '@material-ui/icons/Alarm';
 import { makeStyles } from '@material-ui/styles';
 
 const useStyles = makeStyles({
-  headerStyle: {
-    fontStyle: 'oblique',
-    fontWeight: 'bold'
-  },
   buttonStyle: {
     background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
     border: 0,
@@ -29,27 +25,7 @@ function App() {
 
   return (
     <div>
-      <div>
-        <Button variant="outlined" color="secondary" size="large" endIcon={<DeleteIcon />}>
-          Primary
-        </Button>
-        <br/>
-        <Button className={classes.buttonStyle} variant="contained" size="large" endIcon={<DeleteIcon />}>
-          Primary
-        </Button>
-
-      </div>
-
-      <div>
-        <IconButton color="secondary  " aria-label="add an alarm">
-          <AlarmIcon />
-        </IconButton>
-      </div>
-
-      <Typography className={classes.headerStyle} variant="h1" component="h2" color="primary">
-        <span>This is span h1 Tag</span>
-        <p>This is paragraph h1</p>
-      </Typography>
+      
     </div>
   );
 }
